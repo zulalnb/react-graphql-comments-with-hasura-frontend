926fb8edfd58138058cecdefecb9485785e326c2
Zülal <zllnbn1997@gmail.com>
2024-10-04 14:37:32 +0300

    install and setup apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
